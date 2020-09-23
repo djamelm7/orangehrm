f584472668cd5585e0bb74ac6c6647c7570407e9
Djamel Neguez <41794208+djamelm7@users.noreply.github.com>
2020-09-23 15:31:23 +0200

    new layout for my login template
    header and footer & layout of my new template

---
+++ lo-header.php
@@ -0,0 +1,18 @@
+
+        <!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $cultureElements[0]; ?>" lang="<?php echo $cultureElements[0]; ?>">
+    
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Connection tst </title>
+        
+        <?php include_http_metas() ?>
+        <?php include_metas() ?>
+        <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+        <!-- Library CSS files -->
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+       <link href="<?php echo public_path('../../symfony/web/themes/default/css/style.css')?>" rel="stylesheet" type="text/css"/>      
+
+        
