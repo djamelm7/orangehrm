8de0a9127f41e1806f965f89b1219a7e8c4a71a2
Djamel Neguez <41794208+djamelm7@users.noreply.github.com>
2020-09-23 13:47:56 +0200

    modify setTemplate and setLayout file redirect
    setTemplate("mytemplate","module")
    setLayout("layout")

--- symfony/apps/orangehrm/modules/auth/actions/retryLoginAction.class.php
+++ symfony/apps/orangehrm/modules/auth/actions/retryLoginAction.class.php
@@ -3,8 +3,8 @@
 class retryLoginAction extends sfAction {
 
     public function execute($request) {
-        $this->setLayout('noHeader');        
-        $this->setTemplate('login', 'auth');
+        $this->setLayout('lo-noheader');        
+        $this->setTemplate('con', 'auth');
         $loginForm = new LoginForm();
         $this->message = $this->getUser()->getFlash('message');
         $this->form = $loginForm;
